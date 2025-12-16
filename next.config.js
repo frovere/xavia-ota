452d261d03e3de73cda40fc57ba1e11fd04f7a2a
Felipe Rovere <felipe@lirmi.com>
2025-12-16 10:15:51 -0300

    build: speed up build times

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
   experimental: {
     reactCompiler: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
