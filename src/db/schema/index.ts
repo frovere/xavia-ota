ced31e66f42c18ce3d065611b2ac1156f6c22c98
Felipe Rovere <felipe@lirmi.com>
2025-12-18 23:07:17 -0300

    feat(db): add indexes

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,5 @@
 import { sql } from 'drizzle-orm';
-import { foreignKey, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { foreignKey, index, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const releases = pgTable('releases', {
   id: uuid().defaultRandom().primaryKey().notNull(),
@@ -9,7 +9,11 @@ export const releases = pgTable('releases', {
   commitHash: varchar('commit_hash', { length: 255 }).notNull(),
   commitMessage: varchar('commit_message', { length: 255 }).notNull(),
   updateId: varchar('update_id', { length: 255 }),
-});
+}, (table) => [
+  index('releases_runtime_version_idx').on(table.runtimeVersion),
+  index('releases_timestamp_idx').on(table.timestamp),
+  index('releases_path_idx').on(table.path),
+]);
 
 export const releasesTracking = pgTable(
   'releases_tracking',
@@ -32,5 +36,8 @@ export const releasesTracking = pgTable(
       foreignColumns: [releases.id],
       name: 'fk_release',
     }).onDelete('cascade'),
+    index('releases_tracking_release_id_idx').on(table.releaseId),
+    index('releases_tracking_download_timestamp_idx').on(table.downloadTimestamp),
+    index('releases_tracking_platform_idx').on(table.platform),
   ],
 );
